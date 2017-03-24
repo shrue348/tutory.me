7f187c02c126ebbc68237591166679fb51a48448
shrue348 <shure348@gmail.com>
2017-03-24 10:41:46 +0300

    tabs, adaptive, animations

--- js/ready.js
+++ js/ready.js
@@ -32,13 +32,16 @@ $(function() {
 
 	$('.select').selectbox();
 
+	$('.main_menu__toggle').on('click', function(){
+		$(this).next().toggle(200);
+	})
 });
+
 
 
 $(window).scroll(function(e){
 	parallax();
 });
-
 function parallax(){
 	var scrolled = $(window).scrollTop(),
 		block2top = parseFloat($('.block2 .scrollback').css('top')),
@@ -52,59 +55,75 @@ function parallax(){
 	$('.block6 .scrollback').css({'transform': 'translateY('+(- block2top + scrolled - 3600)*0.17+'px)'});	
 }
 
+
+
 $(function () {
 	'use strict';
+	$('.tabs_wrap').each(function(){
+		var $container = $(this),
+			$swipeTabsContainer = $(this).find('.swipe-tabs'),
+			$swipeTabs =  $(this).find('.swipe-tab'),
+			$swipeTabsContentContainer =  $(this).find('.swipe-tabs-container'),
+			swipeTabsLength = $swipeTabs.length,
+			currentIndex = 0,
+			activeTabClassName = 'active-tab';
 
-	var $swipeTabsContainer = $('.swipe-tabs'),
-		$swipeTabs = $('.swipe-tab'),
-		$swipeTabsContentContainer = $('.swipe-tabs-container'),
-		currentIndex = 0,
-		activeTabClassName = 'active-tab';
+		$swipeTabsContainer.on('init', function(event, slick) {
+			$swipeTabsContentContainer.removeClass('invisible');
+			$swipeTabsContainer.removeClass('invisible');
+			currentIndex = slick.getCurrent();
+			$swipeTabs.removeClass(activeTabClassName);
+	       	$container.find('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
+		});
 
-	$swipeTabsContainer.on('init', function(event, slick) {
-		$swipeTabsContentContainer.removeClass('invisible');
-		$swipeTabsContainer.removeClass('invisible');
+		$swipeTabsContainer.slick({
+			slidesToShow: swipeTabsLength,
+			slidesToScroll: 1,
+			arrows: false,
+			infinite: false,
+			swipeToSlide: true,
+			touchThreshold: 10,
+			responsive: [
+			    {
+			      breakpoint: 768,
+			      settings: {
+			        arrows: false,
+			        slidesToShow: 2.5
+			      }
+			    },
+			    {
+			      breakpoint: 480,
+			      settings: {
+			        arrows: false,
+			        slidesToShow: 1.5
+			      }
+			    }
+			  ]
+		});
 
-		currentIndex = slick.getCurrent();
-		$swipeTabs.removeClass(activeTabClassName);
-       	$('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
-	});
+		$swipeTabsContentContainer.slick({
+			asNavFor: $swipeTabsContainer,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			infinite: false,
+			swipeToSlide: true,
+	   		draggable: true,
+			touchThreshold: 10
+		});
 
-	$swipeTabsContainer.slick({
-		//slidesToShow: 3.25,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		arrows: false,
-		infinite: false,
-		swipeToSlide: true,
-		touchThreshold: 10
-	});
+		$swipeTabs.on('click', function(event) {
+	        currentIndex = $(this).data('slick-index');
+	        $swipeTabs.removeClass(activeTabClassName);
+	        $container.find('.swipe-tab[data-slick-index=' + currentIndex +']').addClass(activeTabClassName);
+	        $swipeTabsContainer.slick('slickGoTo', currentIndex);
+	        $swipeTabsContentContainer.slick('slickGoTo', currentIndex);
+	    });
 
-	$swipeTabsContentContainer.slick({
-		asNavFor: $swipeTabsContainer,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		infinite: false,
-		swipeToSlide: true,
-   		draggable: true,
-		touchThreshold: 10
-	});
-
-
-	$swipeTabs.on('click', function(event) {
-        // gets index of clicked tab
-        currentIndex = $(this).data('slick-index');
-        $swipeTabs.removeClass(activeTabClassName);
-        $('.swipe-tab[data-slick-index=' + currentIndex +']').addClass(activeTabClassName);
-        $swipeTabsContainer.slick('slickGoTo', currentIndex);
-        $swipeTabsContentContainer.slick('slickGoTo', currentIndex);
-    });
-
-    //initializes slick navigation tabs swipe handler
-    $swipeTabsContentContainer.on('swipe', function(event, slick, direction) {
-    	currentIndex = $(this).slick('slickCurrentSlide');
-		$swipeTabs.removeClass(activeTabClassName);
-		$('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
+	    $swipeTabsContentContainer.on('swipe', function(event, slick, direction) {
+	    	currentIndex = $(this).slick('slickCurrentSlide');
+			$swipeTabs.removeClass(activeTabClassName);
+			$container.find('.swipe-tab[data-slick-index=' + currentIndex + ']').addClass(activeTabClassName);
+		});
 	});
 });
